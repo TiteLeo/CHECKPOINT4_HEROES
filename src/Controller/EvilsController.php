6c5c97c8361587f180069f0008e4a7cf4fc8c6ba
Eleonore <eleonoregouze33@gmail.com>
2022-02-02 22:56:14 +0100

    Create Evils's page with method and view. Front not finished

--- src/Controller/EvilsController.php
+++ src/Controller/EvilsController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Character;
+use App\Repository\CharacterRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +14,14 @@ class EvilsController extends AbstractController
     /**
      * @Route("/evils", name="evils")
      */
-    public function index(): Response
+    public function index(ManagerRegistry $managerRegistry, CharacterRepository $characterRepository): Response
     {
+        $characterRepository = $managerRegistry->getRepository(Character::class);
+        $characters = $characterRepository->findBy(['isNasty' => true]);
+
         return $this->render('evils/index.html.twig', [
-            'controller_name' => 'EvilsController',
+            'evils' => $characterRepository->findAll(),
+            'characters' => $characters
         ]);
     }
 }
