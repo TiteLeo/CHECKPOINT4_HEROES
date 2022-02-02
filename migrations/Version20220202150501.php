a8fd9280093edabdf81c91ac9e3c84a6dfb203e1
Eleonore <eleonoregouze33@gmail.com>
2022-02-02 16:19:27 +0100

    Create HeroesFixtures

--- migrations/Version20220202150501.php
+++ migrations/Version20220202150501.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220202100602 extends AbstractMigration
+final class Version20220202150501 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,11 @@ final class Version20220202100602 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE `character` (id INT AUTO_INCREMENT NOT NULL, real_name VARCHAR(255) DEFAULT NULL, sex VARCHAR(255) DEFAULT NULL, hero_name VARCHAR(255) DEFAULT NULL, description LONGTEXT DEFAULT NULL, story LONGTEXT DEFAULT NULL, is_nasty TINYINT(1) NOT NULL, strength VARCHAR(255) NOT NULL, weakness VARCHAR(255) NOT NULL, url LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE character_power (character_id INT NOT NULL, power_id INT NOT NULL, INDEX IDX_5549E6C21136BE75 (character_id), INDEX IDX_5549E6C2AB4FC384 (power_id), PRIMARY KEY(character_id, power_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE character_equipment (character_id INT NOT NULL, equipment_id INT NOT NULL, INDEX IDX_546877B81136BE75 (character_id), INDEX IDX_546877B8517FE9FE (equipment_id), PRIMARY KEY(character_id, equipment_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE equipment (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE power (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE character_power ADD CONSTRAINT FK_5549E6C21136BE75 FOREIGN KEY (character_id) REFERENCES `character` (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE character_power ADD CONSTRAINT FK_5549E6C2AB4FC384 FOREIGN KEY (power_id) REFERENCES power (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE character_equipment ADD CONSTRAINT FK_546877B81136BE75 FOREIGN KEY (character_id) REFERENCES `character` (id) ON DELETE CASCADE');
@@ -31,10 +34,14 @@ final class Version20220202100602 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE character_power DROP FOREIGN KEY FK_5549E6C21136BE75');
+        $this->addSql('ALTER TABLE character_equipment DROP FOREIGN KEY FK_546877B81136BE75');
+        $this->addSql('ALTER TABLE character_equipment DROP FOREIGN KEY FK_546877B8517FE9FE');
+        $this->addSql('ALTER TABLE character_power DROP FOREIGN KEY FK_5549E6C2AB4FC384');
+        $this->addSql('DROP TABLE `character`');
         $this->addSql('DROP TABLE character_power');
         $this->addSql('DROP TABLE character_equipment');
-        $this->addSql('ALTER TABLE `character` CHANGE real_name real_name VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE sex sex VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE hero_name hero_name VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE description description LONGTEXT DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE story story LONGTEXT DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE strength strength VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE weakness weakness VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE url url LONGTEXT DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE equipment CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE type type VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE description description LONGTEXT DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE power CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE description description LONGTEXT DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('DROP TABLE equipment');
+        $this->addSql('DROP TABLE power');
     }
 }
--- src/DataFixtures/HeroesFixtures.php
+++ src/DataFixtures/HeroesFixtures.php
@@ -2,16 +2,40 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Character;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class HeroesFixtures extends Fixture
+class HeroesFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $character = new Character();
+        $character->setRealName('Bruce Wayne');
+        $character->setSex('Homme');
+        $character->setHeroName('Batman');
+        $character->setDescription('Batman chasse les malfaiteurs la nuit et les saisit d\'effroi. Il est pret à tout pour atteindre son but avec une seule règle: ne jamais tuer.');
+        $character->setStory('Milliardaire et orphelin, il cherche à développer ses facultés afin de combattre le crime dans la ville de Gotham City.');
+        $character->setIsNasty(false);
+        $character->setStrength('Son intelligence et sa fortune qui lui permettent d\'avoir des équipements de pointe.');
+        $character->setWeakness('Son humanité, sa moralité et sa vulnérabilité');
+        $character->setUrl('https://voyage-onirique.com/wp-content/uploads/2021/03/batman-throne-4k-wallpaper.jpg');
+        $manager->persist($character);
+
+        $character2 = new Character();
+        $character2->setRealName('Clark Kent');
+        $character2->setSex('Homme');
+        $character2->setHeroName('Superman');
+        $character2->setDescription('Superman est aussi appelé l\'homme d\'acier.');
+        $character2->setStory('Une partie des pouvoirs de Superman lui sont conférés par le Soleil jaune de la Terre dont il absorbe les radiations, ce qui lui donne de supers pouvoirs.');
+        $character2->setIsNasty(false);
+        $character2->setStrength('Une super-force, une super-vitesse, une super-endurance, une super-résistance et de supers 5 sens!');
+        $character2->setWeakness('La kryptonite, la magie, perte de pouvoirs sous les radiations d\'un soleil rouge...');
+        $character2->setUrl('https://getwallpapers.org/wp-content/uploads/2021/08/Superman-Wallpaper-1.jpg');
+        $manager->persist($character2);
 
         $manager->flush();
     }
+
 }
