8f7f6191ba197f2b0d1bb05ffa61e4d24000f755
Eleonore <eleonoregouze33@gmail.com>
2022-02-02 12:55:19 +0100

    Create controllers and swiper in homepage

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,7 @@ Encore
      * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('swiper', './assets/swiper.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
