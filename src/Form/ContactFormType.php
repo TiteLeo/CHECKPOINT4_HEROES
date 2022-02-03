3881f22dd26344cc0242d1a5ddc0b729aa81f75f
Eleonore <eleonoregouze33@gmail.com>
2022-02-03 18:29:17 +0100

    Update ContactFormType

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -3,7 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Message;
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +16,26 @@ class ContactFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('mail')
-            ->add('text')
-            ->add('choice')
+            ->add('name',TextType::class, [
+                'label'   => 'Tes petits noms et prénoms',
+            ])
+            ->add('mail', TextType::class, [
+                'label'   => 'Email',
+            ])
+            ->add('choice', ChoiceType::class, [
+                'label'   => 'Dis-moi tout!',
+                'choices' => [
+                    'Ce site est Super Top'       => 'Ce site est Super',
+                    'Ce site est Super Vilain'    => 'Ce site est Super Vilain',
+                    'Juste pour dire un mot doux' => 'Juste pour dire un mot doux',
+                ]
+            ])
+            ->add('text', TextareaType::class, [
+                'label'   => 'Ton mot doux',
+                'attr'    => [ 'rows' => "8",
+                    ]
+            ])
+
         ;
     }
 
